0f4894f61023fe1ea8f75020c806279c77667b2f
Mario <moraorviz@gmail.com>
2018-12-01 01:43:38 +0100

    ajax psot

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     let boton = $("#cargarEntidades");
     let botonLista = $("#cargarLista");
     let botonPOST = $("#introducirEntidad");
-    let botonSubmitEntidad = $("#submitEntidad");
+
     boton.click(() => {
         $(event.target).prop("disabled", true);
         pedirEntidades();
@@ -15,11 +15,50 @@ $(document).ready(function () {
     });
     botonPOST.click(() => {
         $(event.target).prop("disabled", true);
-        $("#introducirEntidadForm").toggle();
+        $("#entidadForm").toggle();
     });
-    botonSubmitEntidad.click(()=> {
-        introducirEntidad();
-    });
+});
+
+$("#entidadForm").submit(function (event) {
+    event.preventDefault();
+    if (validarFormulario()) {
+        console.log("Ejecutando submit");
+        let div = $("<div></div>")
+        let $form = $(this),
+            titulo = $form.find("input[name='name']").val(),
+            autor = $form.find("input[name='author']").val(),
+            isbnF = $form.find("input[name='isbn']").val(),
+            fechaPub = $form.find("input[name='datePublished']").val(),
+            url = $form.attr("action");
+
+
+
+        let entityJSON = {
+            "@context": "http://schema.org",
+            "@type": "Book",
+            "name": titulo,
+            "author":
+            {
+                "@type": "Person",
+                "name": autor
+            },
+            "datePublished": fechaPub,
+            "isbn": isbn
+        }
+
+        let arr = { '@context': 'http://schema.org', '@type': 'Book', name: titulo, author: { '@type': 'Person', name: autor }, datePublished: fechaPub, isbn: isbnF };
+        $.ajax({
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            url: url,
+            data: JSON.stringify(arr),
+            success: function (data) {
+                console.log(data.title);
+                console.log(data.article);
+            },
+            dataType: "json"
+        });
+    }
 });
 
 function pedirEntidades() {
@@ -28,20 +67,20 @@ function pedirEntidades() {
     });
 }
 
-function pedirLista () {
+function pedirLista() {
     $.get(`http://127.0.0.1:5000/python-api/v1/book`, (respuesta) => {
         console.log(respuesta);
         $.each(respuesta, function (index, value) {
-        let div = $("<div></div>")
-        console.log(index);
-        div.append(`<br>`);
-        div.append(`<p> Autor : ${value.author.name}</p>`);
-        div.append(`<p> Fecha de publicación : ${value.datePublished}</p>`);
-        div.append(`<p> isbn : ${value.isbn}</p>`);
-        div.append(`<p> Título : ${value.name}</p>`);
-        div.append(`<p> Ruta del servicio : ${value.url}</p>`);
-        $("body").append(div);
-    });
+            let div = $("<div></div>")
+            console.log(index);
+            div.append(`<br>`);
+            div.append(`<p> Autor : ${value.author.name}</p>`);
+            div.append(`<p> Fecha de publicación : ${value.datePublished}</p>`);
+            div.append(`<p> isbn : ${value.isbn}</p>`);
+            div.append(`<p> Título : ${value.name}</p>`);
+            div.append(`<p> Ruta del servicio : ${value.url}</p>`);
+            $("body").append(div);
+        });
     });
 }
 
@@ -57,44 +96,40 @@ function drawEntidades(entidades) {
     });
 }
 
-function introducirEntidad() {
-    validarFormulario();
-    console.log('POST');
-}
 
 function validarFormulario() {
     let valido = true;
-    titulo = formularioEntidad.name.value;
+    titulo = entidadForm.name.value;
     if (titulo == '') {
         alert('Es obligatorio poner el nombre');
         valido = false;
     }
-    autor = formularioEntidad.author.value;
+    autor = entidadForm.author.value;
     if (valido && autor == '') {
         alert('Es obligatorio poner el autor');
         valido = false;
     }
-    isbn = formularioEntidad.isbn.value;
+    isbn = entidadForm.isbn.value;
     let isbnRe = /\d{1}-\d{4}-\d{4}-\d{1}/;
     let isbnOK = isbnRe.exec(isbn);
-    if (valido && isbn =='') {
+    if (valido && isbn == '') {
         alert('Es obligatorio poner el isbn');
         valido = false;
     } else if (valido && !isbnOK) {
         alert('El isbn debe tener el formato indicado');
         valido = false;
     }
-    fecha = formularioEntidad.datePublished.value;
+    fecha = entidadForm.datePublished.value;
     let fechaRe = /\d{4}[/](0+[1-9]|1[012])[/](0+[1-9]|[1|2]\d|3[01])/;
     let fechaOK = fechaRe.exec(fecha);
-    if (valido && fecha =='') {
+    if (valido && fecha == '') {
         alert('Es obligatorio poner la fecha de publicación');
         valido = false;
     } else if (valido && !fechaOK) {
         alert('La fecha debe tener el formato indicado');
         valido = false;
     }
-    event.returnValue = valido;
+    return valido;
 }
 
 
